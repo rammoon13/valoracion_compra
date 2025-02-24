6c2054bbfa30bc40f3ffde9c09b021c9ea2c58e7
rammoon13 <herreraroblesramon@gmail.com>
2025-02-24 22:59:51 +0100

    elecciones

--- models/valoracion_compra.py
+++ models/valoracion_compra.py
@@ -5,8 +5,8 @@ class ValoracionCompra(models.Model):
     _name = 'valoracion.compra'
     _description = 'Valoración de Compra'
 
-    trabajador = fields.Char(string="Nombre del Trabajador", required=True)
-    empresa = fields.Char(string="Nombre de la Empresa", required=True)
+    trabajador = fields.Many2one('hr.employee', string="Nombre del Trabajador", required=True)
+    empresa = fields.Many2one('res.partner', string="Nombre de la Empresa", required=True, domain=[('is_company', '=', True)])
     fecha_compra = fields.Date(string="Fecha de la Compra", required=True)
     comentario = fields.Text(string="Comentario sobre el desempeño", required=True)
     linea_ids = fields.One2many('valoracion.compra.line', 'compra_id', string="Artículos Vendidos")
@@ -16,5 +16,5 @@ class ValoracionCompraLine(models.Model):
     _description = 'Línea de Artículos Vendidos en la Valoración de Compra'
 
     compra_id = fields.Many2one('valoracion.compra', string="Compra", ondelete='cascade')
-    producto = fields.Char(string="Producto", required=True)
+    producto = fields.Many2one('product.product', string="Producto", required=True)
     cantidad = fields.Integer(string="Cantidad", required=True)
